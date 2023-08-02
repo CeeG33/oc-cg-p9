e2463201bd85d7a823ddd1b87bd08ec42f082dec
CeeG33 <ciran.gurbuz@gmail.com>
2023-08-02 09:21:38 +0200

    Ajout du fichier sqlite3

--- litreview/admin.py
+++ litreview/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from litreview.models import User, Ticket, Review, UserFollows
 
-# Register your models here.
+models = [User, Ticket, Review, UserFollows]
+
+admin.site.register(models)
